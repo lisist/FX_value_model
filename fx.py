5789fabcd5d7d05b0d5379e829459985b1642186
lisist <46858327+lisist@users.noreply.github.com>
2022-01-17 16:53:26 +0900

    Update fx.py

--- fx.py
+++ fx.py
@@ -5,35 +5,6 @@ from datetime import datetime
 from sklearn.linear_model import LinearRegression
 from fredapi import Fred
 
-fred_api_key = 'a2524d0d1124080eb016f900416e4df2'
-
-def twin_deficit():
-    usd_nominal_effective = pd.read_excel('total.xlsx', sheet_name='USDEffective', index_col='Date')
-    us_twin_defic = -pd.read_excel('total.xlsx', sheet_name='US_TWIN_DEFI', index_col='Date') 
-
-    merged_df = usd_nominal_effective.merge(us_twin_defic,left_index=True,right_index=True)
-
-
-    ## 참고 : Python - 2축 그래프 그리기
-    fig = plt.figure(figsize=(7,4))
-
-    ax1 = fig.subplots()
-    ax2 = ax1.twinx()
-
-    # ax1.set_title('dd') 
-
-    line1 = ax1.plot(merged_df['BISNUSN index'].shift(-10))
-    line2 = ax2.plot(merged_df['.TWIN G index'],linestyle="--",color='r')
-
-    lines = line1 + line2
-    plt.legend(lines,['Nominal Effective US dollar (ls, 10M lagged)','US : Sum of Current Account and Fiscal Balance (rs)'],loc=2)
-
-    ax1.set_ylim(60,160)
-    ax2.set_ylim(-20,4)
-    ax2.set_ylabel("%GDP")
-
-    plt.tight_layout()
-    plt.savefig("./images/US_TwinD_USD.png")
 
 
 def z_score(data,time_line):
@@ -127,40 +98,7 @@ def basic_model():
 
 
 
-# def real_yield_differentials(api_key=fred_api_key):
-#     fred = Fred(api_key)
-#     rates_tickers = ['IRLTLT01SEM156N','IRLTLT01AUM156N','IRLTLT01NOM156N','IRLTLT01CAM156N','IRLTLT01EZM156N',
-#             'IRLTLT01GBM156N','IRLTLT01JPM156N','IRLTLT01NZM156N','IRLTLT01CHM156N']
-#     cpi_tickers = ['SWECPIALLMINMEI','AUSCPIALLQINMEI','NORCPIALLMINMEI','CPALCY01CAM661N','CP0000EZ19M086NEST',
-#             'GBRCPIALLMINMEI','JPNCPIALLMINMEI','NZLCPIALLQINMEI','CHECPIALLMINMEI']
-
-#     rates_names = ['SW','AU','NO','CA','EU','GB','JP','NZ','CF']
-
-#     rates_data_df = pd.read_csv('./data/10yearyield.csv',index_col='Unnamed: 0',parse_dates=True)
-    
-
-#     df_rates = pd.DataFrame(fred.get_series(rates_tickers[0])['1994-01-01':])
-    
-#     if rates_data_df.index[-1] != df_rates.index[-1]:
-#         for i in rates_tickers[1:]:
-#             df_rates = df_rates.merge(pd.DataFrame(fred.get_series(i)['1994-01-01':]),left_index=True, right_index=True, how='outer')
-        
-#         df_rates.to_csv('./data/10yearyield.csv')
-#         rates_data_df = df_rates
-    
-#     rates_data_df.columns = rates_names
-
-#     df_cpi = pd.DataFrame(fred.get_series(cpi_tickers[0])['1993-01-01':])
-
-#     for i in cpi_tickers[1:]:
-#         df_cpi = df_cpi.merge(pd.DataFrame(fred.get_series(i)['1993-01-01':]),left_index=True, right_index=True,how='outer')
-        
-    
-#     print(df_cpi)
-
 
         
 if __name__ == "__main__":
-    twin_deficit()
     basic_model()
-    # real_yield_differentials()
